1c77d393ccd7309391191fc92e6461e3b19e119e
emilpoppler134 <emilpoppler@Emils-Air.lan>
2024-02-11 21:28:20 +0100

    Temporary remove the cart counter in the nav

--- src/components/NavLayout.vue
+++ src/components/NavLayout.vue
@@ -290,7 +290,7 @@ const open = ref(false)
                 <router-link to="/cart">
                   <span class="group -m-2 flex items-center p-2">
                     <ShoppingBagIcon class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                    <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                    <!-- <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span> -->
                     <span class="sr-only">items in cart, view bag</span>
                   </span>
                 </router-link>
